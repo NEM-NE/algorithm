991236b433d88504730081c903688f0debdb9c99
NEM-NE <55152516+NEM-NE@users.noreply.github.com>
2021-06-30 01:05:15 +0900

    feat(pro) : 가장 큰수 (성공)(조금 쉬움)

---
+++ Programmers/가장 큰수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    // 12 : 27 37
+    public String solution(int[] numbers) {
+        
+        Integer b[] = Arrays.stream(numbers).boxed().toArray(Integer[]::new); 
+        Arrays.sort(b, new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2){
+                String str1 = o1 + "";
+                String str2 = o2 + "";
+                
+                int a = Integer.parseInt(str1 + str2);
+                int b = Integer.parseInt(str2 + str1);
+                
+                return b - a;
+            }
+        });
+        
+        String ans = "";
+
+        for(int i = 0; i < b.length; i++){
+            if(ans.equals("") && b[i] == 0) return "0";
+            ans += b[i];
+        }
+
+        return ans;
+    }
+}
