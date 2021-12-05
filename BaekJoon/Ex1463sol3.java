79f26dc62f4652b3674304d83ddf54fdf27b2596
NEM-NE <dlatqdlatq@mju.ac.kr>
2021-12-05 11:00:00 +0900

    solve: BJ_1463_sol3

---
+++ BaekJoon/Ex1463sol3.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Ex1463sol3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int size = Integer.parseInt(br.readLine());
+        int[] arr = new int[size + 1];
+
+        arr[1] = 0;
+
+        for(int i = 2; i <= size; i++) {
+
+            arr[i] = arr[i - 1] + 1;
+
+            if(i % 3 == 0){
+                arr[i] = Math.min(arr[i / 3] + 1, arr[i]);
+            }
+
+            if(i % 2 == 0){
+                arr[i] = Math.min(arr[i / 2] + 1, arr[i]);
+            }
+
+        }
+
+        sb.append(arr[size]);
+        System.out.println(sb);
+    }
+}
