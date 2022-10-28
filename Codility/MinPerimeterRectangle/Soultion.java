2c754b27c2232f3030f6787c8cac195f5966d0be
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-10-29 00:13:09 +0900

    solve: codi_MinPerimeterRectangle_sol1_33min

---
+++ Codility/MinPerimeterRectangle/Soultion.java
@@ -0,0 +1,30 @@
+package MinPerimeterRectangle;
+
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+public class Solution {
+  public int solution(int N) {
+    int result = 2 * (N + 1);
+
+    int i = 2;
+    int limit = N/i;
+    while(i <= limit){
+      if(N%i == 0){
+        result = Math.min(result, getPerimeter(N/i, i));
+      }
+
+      i++;
+      limit = N/i;
+    }
+
+    return result;
+  }
+
+  public int getPerimeter(int a, int b){
+    return (a + b) * 2;
+  }
+}
