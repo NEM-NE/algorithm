b5521904e17a863b30c4390ac52bde64fc97d58c
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-10-29 17:24:18 +0900

    solve: codi_Distinct_sol1_2min

---
+++ Codility/Distinct/Soultion.java
@@ -0,0 +1,17 @@
+package Distinct;
+
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+public class Solution {
+  public int solution(int[] A) {
+    Set<Integer> set = new HashSet<>();
+    for(int num : A){
+      set.add(num);
+    }
+
+    return set.size();
+  }
+}
